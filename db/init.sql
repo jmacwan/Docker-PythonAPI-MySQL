88c451e0eaeb448d9cd45662cfaa82a78aa80abf
jmacwan <45224645+jmacwan@users.noreply.github.com>
2026-05-24 19:24:04 -0400

    Create init.sql

---
+++ db/init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS mydb;
+
+USE mydb;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+INSERT INTO users (name) VALUES ("Alice"), ("Bob"), ("Charlie");
