4a7b0cb42145cd8a4a44c63ffd91380abaafdfe8
jmacwan <45224645+jmacwan@users.noreply.github.com>
2026-05-24 19:22:11 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
+from fastapi import FastAPI
+import mysql.connector
 
+app = FastAPI()
+
+def get_connection():
+    return mysql.connector.connect(
+        host="mysql",
+        user="root",
+        password="rootpassword",
+        database="mydb"
+    )
+
+@app.get("/")
+def root():
+    return {"message": "Python API + MySQL via Docker!"}
+
+@app.get("/users")
+def get_users():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, name FROM users;")
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return {"users": rows}
